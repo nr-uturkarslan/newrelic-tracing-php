50844fd156984d2ededf44abbb48707f5728f320
uturkarslan <ugurturkarslan@gmail.com>
2022-09-02 09:33:02 +0200

    Fix dir

--- infra/helpers/vm/00_user_data.sh
+++ infra/helpers/vm/00_user_data.sh
@@ -10,6 +10,6 @@ cd app
 git clone https://github.com/nr-uturkarslan/newrelic-tracing-php.git
 
 # Run setup
-cd newrelic-tracing-php
-bash run_setup.sh \
+cd newrelic-tracing-php/infra/helpers/vm
+bash 01_run_setup.sh \
   --license-key <YOUR_NEWRELIC_LICENCE_KEY>
