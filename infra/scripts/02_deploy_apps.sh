961d24c5188e4ea6a5c19c8c9cbd3398f384836a
uturkarslan <ugurturkarslan@gmail.com>
2022-09-02 09:25:12 +0200

    Add docker network

--- infra/scripts/02_deploy_apps.sh
+++ infra/scripts/02_deploy_apps.sh
@@ -6,25 +6,36 @@
 
 ### Set variables
 
+# Docker
+dockerNetwork="php-tracing"
+
 # Proxy
 declare -A proxy
 proxy["name"]="proxy"
 proxy["imageName"]="proxy-php"
 proxy["namespace"]="proxy"
 proxy["port"]=80
-#########
+######
 
-####################
-### Build & Push ###
-####################
+### Docker setup ###
+docker network create \
+  --driver bridge \
+  $dockerNetwork
+######
+
+### Build ###
 
 # Proxy
 docker build \
   --tag "${DOCKERHUB_NAME}/${proxy[imageName]}" \
   "../../apps/proxy/."
+######
 
+### Run ###
 docker run \
   --rm \
+  --network $dockerNetwork \
   --name "${proxy[name]}" \
   -p ${proxy[port]}:${proxy[port]} \
   "${DOCKERHUB_NAME}/${proxy[imageName]}"
+######
